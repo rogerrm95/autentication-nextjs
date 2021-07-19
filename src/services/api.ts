edfdceeb8ab246198fdea3a74b5088b9a794b31b
rogerrm95 <rogerrm@live.com>
2021-07-18 21:45:02 -0300

    Add: Refresh Token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,42 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
         Authorization: `Bearer ${cookies['petmania.token']}`
+    }
+})
+
+api.interceptors.response.use(response => response, (responseError: AxiosError) => {
+    if (responseError.response.status === 401) {
+        if (responseError.response.data?.code === 'token.expired') {
+            // Renovar o TOKEN
+            cookies = parseCookies()
+
+            const { 'petmania.refreshToken': refreshToken } = cookies
+
+            api.post('/refresh', {
+                refreshToken
+            }).then(res => {
+                const { token } = res.data
+
+                setCookie(undefined, 'petmania.token', token, {
+                    maxAge: 60 * 60 * 24 * 30,
+                    path: '/'
+                })
+                setCookie(undefined, 'petmania.refreshToken', res.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30,
+                    path: '/'
+                })
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`
+            })
+
+        } else {
+            // deslogar o usuario
+        }
     }
 })
